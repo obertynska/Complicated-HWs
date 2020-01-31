319f2a7de62f3076553572f29fb6d5dd2043928f
obertynska <obertynska11@gmail.com>
2020-01-31 20:57:01 +0200

    the function was changed

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 'use strict';
 
 let func = function (i) {
-    if (typeof i !== 'string') {
-        alert("нестрока");
-    };
 
-    if (i.length > 30) {
-       console.log( i.trim().substring(0, 30) + `...`);
+    if (typeof i !== 'string') {
+        console.log("нестрока");
+    } else if (i.length > 30) {
+        console.log(i.trim().substring(0, 30) + `...`);
     } else {
         console.log(i.trim());
     }
 
 };
 
-func( "55555555555555555555555555555555555555555555555555555555555555555555555555555");
+func();
+
+
+
+
